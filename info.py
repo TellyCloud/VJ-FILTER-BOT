8a0598bb56b73e653a59dbf95506ebc9dec1483b
TellyCloud <156745846+TellyCloud@users.noreply.github.com>
2024-09-13 18:50:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ SHORTLINK_API = environ.get('SHORTLINK_API', '1363e5dd5ec8084dd9df83fcb53d436870
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
-PORT = environ.get("PORT", "8080")
+PORT = environ.get("PORT", "8081")
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello My Dear Friends ❤️')
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
