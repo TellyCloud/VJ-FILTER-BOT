0d15d1d6d5c78bade89381d44b3b7edb0b834405
TellyCloud <156745846+TellyCloud@users.noreply.github.com>
2024-09-18 07:05:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ async def start():
         print("Restarted All Clone Bots.")
     app = web.AppRunner(await web_server())
     await app.setup()
-    bind_address = "206.1.53.78"
+    bind_address = "103.248.82.182"
     await web.TCPSite(app, bind_address, PORT).start()
     await idle()
 
